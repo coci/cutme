c331ad0c984dfcf212278795a07b06dfdf912fce
soroush <soroush.safari1992@gmail.com>
2025-10-31 23:19:44 +0330

    add ShortenerService

---
+++ internal/services/shortener_service.go
@@ -0,0 +1,69 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/coci/cutme/internal/core/domain"
+	"github.com/coci/cutme/internal/core/ports"
+	"github.com/coci/cutme/pkg/config"
+	"github.com/speps/go-hashids/v2"
+)
+
+type ShortenerService struct {
+	ports.Shortener
+
+	linkRepo    ports.LinkRepo
+	idGenerator ports.IDGenerator
+	config      *config.Config
+}
+
+func NewShortenerService(linkRepo ports.LinkRepo, idGenerator ports.IDGenerator, config *config.Config) *ShortenerService {
+	return &ShortenerService{
+		linkRepo:    linkRepo,
+		idGenerator: idGenerator,
+		config:      config,
+	}
+}
+
+func (s ShortenerService) Shorten(url string) (string, error) {
+	code := s.MakeUniqueCode()
+
+	data := domain.Link{
+		Code:      code,
+		Link:      url,
+		CreatedAt: s.clock(),
+	}
+
+	err := s.linkRepo.Save(data)
+
+	if err != nil {
+		return "", err
+	}
+
+	return code, nil
+}
+
+func (s ShortenerService) Resolve(code string) (string, error) {
+	return "google.com", nil
+}
+
+func (s ShortenerService) MakeUniqueCode() string {
+	newBaseCode := s.idGenerator.NextID()
+
+	hashId := hashids.HashIDData{
+		Salt:      s.config.HashCfg.HashSalt,
+		Alphabet:  s.config.HashCfg.HashAlphabet,
+		MinLength: s.config.HashCfg.HashMinLength,
+	}
+	h, _ := hashids.NewWithData(&hashId)
+
+	e, _ := h.Encode([]int{newBaseCode})
+
+	fmt.Println(e)
+
+	return e
+}
+
+func (s ShortenerService) clock() int64 {
+	return 0
+}
