32d1f72c928fef7430050fe717489a43989c0b69
soroush <soroush.safari1992@gmail.com>
2025-10-31 23:19:27 +0330

    add Logger interface to define logging methods

---
+++ internal/core/ports/logger.go
@@ -0,0 +1,13 @@
+package ports
+
+type Field struct {
+	Key   string
+	Value interface{}
+}
+
+type Logger interface {
+	Info(msg string, fields ...Field)
+	Warn(msg string, fields ...Field)
+	Error(msg string, fields ...Field)
+	Fatal(msg string, fields ...Field)
+}
