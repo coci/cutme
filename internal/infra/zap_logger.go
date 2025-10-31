8879225354a9580d40533d038e80b594a47be6b1
soroush <soroush.safari1992@gmail.com>
2025-10-31 23:20:03 +0330

    add ZapLogger implementation for logging

---
+++ internal/infra/zap_logger.go
@@ -0,0 +1,51 @@
+package infra
+
+import (
+	"github.com/coci/cutme/internal/core/ports"
+	"go.uber.org/zap"
+)
+
+type ZapLogger struct {
+	ports.Logger
+
+	logger *zap.Logger
+}
+
+func NewZapLogger() *ZapLogger {
+	logger, _ := zap.NewProduction()
+	return &ZapLogger{
+		logger: logger,
+	}
+}
+
+func (z *ZapLogger) Info(msg string, fields ...ports.Field) {
+	zapFields := make([]zap.Field, len(fields))
+	for i, field := range fields {
+		zapFields[i] = zap.Any(field.Key, field.Value)
+	}
+	z.logger.Info(msg, zapFields...)
+}
+
+func (z *ZapLogger) Warn(msg string, fields ...ports.Field) {
+	zapFields := make([]zap.Field, len(fields))
+	for i, field := range fields {
+		zapFields[i] = zap.Any(field.Key, field.Value)
+	}
+	z.logger.Warn(msg, zapFields...)
+}
+
+func (z *ZapLogger) Error(msg string, fields ...ports.Field) {
+	zapFields := make([]zap.Field, len(fields))
+	for i, field := range fields {
+		zapFields[i] = zap.Any(field.Key, field.Value)
+	}
+	z.logger.Error(msg, zapFields...)
+}
+
+func (z *ZapLogger) Fatal(msg string, fields ...ports.Field) {
+	zapFields := make([]zap.Field, len(fields))
+	for i, field := range fields {
+		zapFields[i] = zap.Any(field.Key, field.Value)
+	}
+	z.logger.Fatal(msg, zapFields...)
+}
