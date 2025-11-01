f0a7370fa4b358ed0e094e216da20a11719473cf
soroush <soroush.safari1992@gmail.com>
2025-11-01 16:38:58 +0330

    update(api): add JSON request/response handling in ShortLink handler

--- internal/adapters/api/shortener_handler.go
+++ internal/adapters/api/shortener_handler.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/coci/cutme/internal/core/domain"
 	"github.com/coci/cutme/internal/core/ports"
 )
 
@@ -16,7 +19,22 @@ func NewHandler(svc ports.Shortener, log ports.Logger) *ShortenerHandler {
 }
 
 func (h *ShortenerHandler) ShortLink(w http.ResponseWriter, r *http.Request) {
+	var shortLink domain.Link
 
+	err := json.NewDecoder(r.Body).Decode(&shortLink)
+	if err != nil {
+		h.Log.Error("failed to decode request body")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	shortLink.Code, _ = h.Svc.Shorten(shortLink.Link)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(shortLink); err != nil {
+		h.Log.Error("failed to encode response")
+	}
 }
 
 func (h *ShortenerHandler) GetLink(w http.ResponseWriter, r *http.Request) {
