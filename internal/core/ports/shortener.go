5bdf7a3f6eb91a576dad194e6bab53103a72b8d0
soroush <soroush.safari1992@gmail.com>
2025-11-05 10:01:32 +0330

    update(ports): modify Shortener.Resolve to return domain.Link instead of string

--- internal/core/ports/shortener.go
+++ internal/core/ports/shortener.go
@@ -1,7 +1,9 @@
 package ports
 
+import "github.com/coci/cutme/internal/core/domain"
+
 type Shortener interface {
 	Shorten(url string) (string, error)
-	Resolve(code string) (string, error)
+	Resolve(code string) (domain.Link, error)
 	MakeUniqueCode() string
 }
